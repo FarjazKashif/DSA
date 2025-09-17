f9ac9ee5bb2d2ea09288c086802969e244c7d06a
Farjaz Kashif <farjazkashif01@gmail.com>
2025-09-18 03:08:05 +0500

    Factorial

--- Recursion/app.java
+++ Recursion/app.java
@@ -9,17 +9,28 @@ class App {
     //     q1(n+1);
     // }
 
-    public static void nNumbers(int n, int sum) {
-        if(n==0) {
-            return;
-        }
-        sum+=n;
-        nNumbers(n-1, sum);
+    // Q2: Print numbers from n → 1.
+    //     Example: Input: n=5 → Output: 5 4 3 2 1
+    // public static void nNumbers(int n, int sum) {
+    //     if(n==0) {
+    //         return;
+    //     }
+    //     sum+=n;
+    //     nNumbers(n-1, sum);
 
-        System.out.println(sum == 15 ? sum : "");
+    //     System.out.println(sum == 15 ? sum : "");
+    // }
+
+    // Q3: Factorial
+    public static int factorial(int n) {
+        if(n==1) {
+            return 1;
+        }
+        return n * factorial(n-1); 
     }
     public static void main(String args[]) {
-        nNumbers(5, 0);
+        // nNumbers(5, 0);
         // q1(1);
+        System.out.println(factorial(5));
     }
 }
