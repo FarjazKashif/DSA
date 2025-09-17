b15c296f331db4db2d77c30528cf47e3ad3c18e6
Farjaz Kashif <farjazkashif01@gmail.com>
2025-09-18 02:58:47 +0500

    Natural numbers Recursion

--- Recursion/app.java
+++ Recursion/app.java
@@ -1,15 +1,25 @@
 class App {
     // Q1: Print numbers from 1 → n.
     //     Example: Input: n=5 → Output: 1 2 3 4 5
-    public static void q1(int n) {
-        if(n==6) {
+    // public static void q1(int n) {
+    //     if(n==6) {
+    //         return;
+    //     }
+    //     System.out.println(n);
+    //     q1(n+1);
+    // }
+
+    public static void nNumbers(int n, int sum) {
+        if(n==0) {
             return;
         }
-        System.out.println(n);
-        q1(n+1);
-    }
+        sum+=n;
+        nNumbers(n-1, sum);
 
+        System.out.println(sum == 15 ? sum : "");
+    }
     public static void main(String args[]) {
-        q1(1);
+        nNumbers(5, 0);
+        // q1(1);
     }
 }
